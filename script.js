114dcb006ec9abac1d8476748d12a0be094a6c2c
LuizFelipeCorra <88915506+LuizFelipeCorra@users.noreply.github.com>
2022-06-08 21:45:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let nome = 'Felipe';
 
 // Crie uma variável que aumentará um número;
 let numero = 1 ;
-console.log( numero + 1)
+console.log( numero + 1);
 
 // Crie uma variável com o ano atual e subtraia número pelo que você guardou;
  let ano = 2022;
@@ -12,7 +12,7 @@ console.log( numero + 1)
 
 // Agora mais no console o resultado da soma das duas variáveis ​​guardadas anteriormente;
 
-let resultado1 = (ano + numero)
+let resultado1 = (ano + numero);
 console.log(resultado1);
 
 //Mais no console a mensagem "Estou no console.log";
@@ -21,7 +21,9 @@ console.log("Estou no console.log");
 // Crie uma variável chamada “quartaFeira” e atribua à ela o valor booleano que representa verdadeiro ou falso e mais saída no console;
 
 let quartaFeira = false;
-console.log(quartaFeira)
+console.log(quartaFeira);
 
 // Exiba no console o tipo de dado da váriavel quartaFeira;
-console.log( typeof quartaFeira)
+console.log( typeof quartaFeira);
+
+console.log("Fim_Codigo");
