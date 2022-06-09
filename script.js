f875937c588e98b31b0affaaefd1e613e1dd82cf
Felipe Correa <luizfelipelopescorrea@gmail.com>
2022-06-08 21:35:23 -0300

    Projeto_Variaveis

---
+++ script.js
@@ -0,0 +1,27 @@
+// Crie uma variável que receba um nome;
+let nome = 'Felipe';
+
+// Crie uma variável que aumentará um número;
+let numero = 1 ;
+console.log( numero + 1)
+
+// Crie uma variável com o ano atual e subtraia número pelo que você guardou;
+ let ano = 2022;
+ let resultado = (ano - numero);
+ console.log(resultado);
+
+// Agora mais no console o resultado da soma das duas variáveis ​​guardadas anteriormente;
+
+let resultado1 = (ano + numero)
+console.log(resultado1);
+
+//Mais no console a mensagem "Estou no console.log";
+console.log("Estou no console.log");
+
+// Crie uma variável chamada “quartaFeira” e atribua à ela o valor booleano que representa verdadeiro ou falso e mais saída no console;
+
+let quartaFeira = false;
+console.log(quartaFeira)
+
+// Exiba no console o tipo de dado da váriavel quartaFeira;
+console.log( typeof quartaFeira)
